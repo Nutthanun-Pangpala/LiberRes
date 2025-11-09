cac05bf73361a56e769bbcd91987641219affaf1
Nutthanun-Pangpala <6531503025@lamduan.mfu.ac.th>
2025-11-09 22:03:34 +0700

    fix: Correctly import intl package and enhance date formatting in ReservationsPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+// [แก้ไข 1] import ที่ถูกต้องคือ 'package:intl' (มี :)
+import 'package:intl/date_symbol_data_local.dart';
 
 // import 'pages/wrapper.dart'; // (อันเก่า)
 import './splash_screen.dart'; // (อันใหม่)
@@ -8,6 +10,10 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // [แก้ไข 2] ย้ายโค้ดมาไว้ที่นี่ครับ! (ก่อน runApp)
+  await initializeDateFormatting('th_TH', null);
+
   runApp(const MyApp());
 }
 
@@ -24,5 +30,6 @@ class MyApp extends StatelessWidget {
       // ‼️ แก้ไข: เปลี่ยน home จาก Wrapper เป็น SplashScreen
       home: const SplashScreen(),
     );
+    // [แก้ไข 3] ลบโค้ดบรรทัดนี้ออกจากที่นี่
   }
 }
